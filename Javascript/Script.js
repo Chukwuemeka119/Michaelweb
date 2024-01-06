3e5d0cbbca22cb4c17ab658565963f39aca8686c
Osita Michael <140946490+Chukwuemeka119@users.noreply.github.com>
2024-01-06 12:58:26 +0100

    Add files via upload

---
+++ Javascript/Script.js
@@ -0,0 +1,6 @@
+// Make The Introduction Disappear and make the real content appear
+function myFunction(){
+    document.getElementById("hide2").style.display = "block";
+    document.getElementById("hide").style.display = "none";
+    document.getElementById("hid").style.backgroundColor = "white";
+}
